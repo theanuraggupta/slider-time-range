cd226fc28b1d054ee982ec17b3c9dc4da6fdbf7e
Anurag Gupta <anurag01gupta@github.com>
2019-01-03 23:13:01 +0530

    slider changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-assessment';
+  selectedRange: any;
+  selectedTime: number;
+  setWidth: number;
+  selectDropdown: any = [
+    {
+        "value": 1,
+        "startRange": 0,
+        "endRange": 100
+    },
+    {
+        "value": 2,
+        "startRange": 100,
+        "endRange": 200
+    },
+    {
+        "value": 3,
+        "startRange": 200,
+        "endRange": 500
+    },
+    {
+        "value": 4,
+        "startRange": 500,
+        "endRange": 1000
+    },
+    {
+        "value": 5,
+        "startRange": 1000,
+        "endRange": 1500
+    }
+  ];
+  constructor(){}
+  ngOnInit() {
+    this.selectedRange = this.selectDropdown[0];
+    this.changedValue(this.selectedRange);
+  }
+
+  changedValue(value){
+    this.selectedTime = ((value.endRange - value.startRange) / 2) + value.startRange;
+    this.changedTime(this.selectedTime);
+  }
+
+  changedTime(value){
+    let range = this.selectedRange.endRange - this.selectedRange.startRange;
+    let s = value - this.selectedRange.startRange;
+    this.setWidth = (s / range) * 100;
+  }
 }
